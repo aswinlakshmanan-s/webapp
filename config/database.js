186c836356cef159b81dcaf3db26ae9be4af700a
aswinlakshmanan-s <sriram.a@northeastern.edu>
2025-02-17 22:00:10 -0500

    changed to secret variables

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ console.log("DEBUG: Process ENV contains DB_PASSWORD?", Boolean(process.env.DB_P
 const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    password,
+    process.env.DB_PASSWORD,
     {
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
