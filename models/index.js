dd60d0ab94dde9224da1b57e5090567a2eec4b9c
aswinlakshmanan-s <sriram.a@northeastern.edu>
2025-02-17 22:30:30 -0500

    trigger workflows with forces

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const initDB = async () => {
     try {
         await sequelize.authenticate();
         console.log('Database connected...');
-        await sequelize.sync({ alter: true }); // Ensure database is up to date
+        await sequelize.sync({ force: true }); // Ensure database is up to date
         console.log('Database synced...');
     } catch (error) {
         console.error('Error connecting to the database:', error);
