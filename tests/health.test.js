30dfddab8273bc604eae5e6aa928c22998a77794
aswinlakshmanan-s <sriram.a@northeastern.edu>
2025-02-23 17:00:17 -0500

    Updated health test and CI workflow jobs

--- tests/health.test.js
+++ tests/health.test.js
@@ -7,7 +7,7 @@ describe('API Tests', () => {
     beforeAll(async () => {
         try {
             await sequelize.authenticate();
-            await sequelize.sync({ alter: true });
+            await sequelize.sync({ force: true });
             console.log("Database connected successfully.");
         } catch (error) {
             console.error("Database connection failed:", error);
